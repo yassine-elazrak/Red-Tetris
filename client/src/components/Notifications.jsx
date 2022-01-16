5d3af4e789efc5359d103cc8acbd83328724052d
alizaynoune <simo.jokir@gmail.com>
2022-01-16 23:42:38 +0100

    update list notif
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import { Menu, Dropdown, Badge } from "antd";
+import { Menu, Dropdown, Badge, Popover, Button } from "antd";
 import { BellOutlined } from '@ant-design/icons';
 
 
 import { red } from '@ant-design/colors';
 
-const { SubMenu } = Menu;
+import "./styles/NotificationsStyled.css";
 
+const { SubMenu } = Menu;
+const MenuItemGroup = Menu.ItemGroup;
 const NotifComponent = () => {
 
     const testnotif = [
@@ -28,12 +30,22 @@ const NotifComponent = () => {
         {
             title: "New message from John Doe",
             time: "3 minutes ago",
-            read: true
+            read: false
         },
         {
             title: "New message from John Doe",
             time: "3 minutes ago",
-            read: true
+            read: false
+        },
+        {
+            title: "New message from John Doe",
+            time: "3 minutes ago",
+            read: false
+        },
+        {
+            title: "New message from John Doe",
+            time: "3 minutes ago",
+            read: false
         },
         {
             title: "New message from John Doe",
@@ -49,75 +61,115 @@ const NotifComponent = () => {
     const [notifs, setNotifs] = useState(testnotif);
 
     const handnotif = (e) => {
+        console.log(e);
+        const id = parseInt(e.key.split("-")[1]);
+        const action = e.key.split("-")[0];
+        console.log(id);
         const newNotifs = notifs.map((notif, key) => {
-            if (parseInt(e.key.split("-")[1]) === key) {
+            if (id === key) {
+                console.log(notif);
                 notif.read = true;
             }
             return notif;
         });
         setNotifs(newNotifs);
-        // console.log(newNotifs[e.key.split("-")[1]]);
     }
+
+    const lenght = notifs.filter(notif => notif.read === false).length;
 
 
     const mapnotifs = notifs.map((notif, key) => {
         return (
             !notif.read ?
-            <SubMenu key={key} expandIcon title={
-                <div style={{ display: "flex", alignItems: "center" }}>
+            <SubMenu key={`notif-${key}`} expandIcon title={
+                <div style={{ height: 30, display: "inline-flex" }}>
                     <div>
                         <p>{notif.title}</p>
-                        <p style={{ fontSize: "12px", color: "#8c8c8c" }}>{notif.time}</p>
+                        <span style={{ fontSize: "12px", color: "#8c8c8c" }}>{notif.time}</span>
                     </div>
                 </div>
             }>
-                <Menu.Item key={'accept-' + key} onClick={handnotif}
-                    style={{
-                        backgroundColor: "#6FCF97",
-                        borderRadius: "3px",
-                        margin: "5px",
+                <MenuItemGroup className="ulNotif">
+                    <Menu.Item key={`accept-${key}`} className="ant-btn ant-btn-primary" onClick={handnotif} style={{
+                        // background: "green",
+                        margin: 5,
+                        textAlign: "center",
+                        alignItems: "center",
+                        display: "inline-flex",
+                        justifyContent: "center",
+                        padding: 0,
                         color: "#fff",
-
                     }}>
-                    Accept
-                </Menu.Item>
-
-                <Menu.Item key={'cancel-' + key} onClick={handnotif} style={{
-                    backgroundColor: red[5],
-                    borderRadius: "3px",
-                    margin: "5px",
-                    color: "#fff",
-                }}>
-                    Cancel
-                </Menu.Item>
+                        Accept
+                    </Menu.Item>
+                    <Menu.Item key={`cancel-${key}`} className="ant-btn ant-btn-primary ant-btn-dangerous" onClick={handnotif} style={{
+                        margin: 5,
+                        textAlign: "center",
+                        alignItems: "center",
+                        display: "inline-flex",
+                        justifyContent: "center",
+                        padding: 0,
+                    }}>
+                        Cancel
+                    </Menu.Item>
+                </MenuItemGroup>
             </SubMenu>
             :
-            <SubMenu key={key} expandIcon disabled title={
-                <div style={{ display: "flex", alignItems: "center" }}>
+            <Menu.Item key={`notif-${key}`} disabled>
+                <div style={{ height: 30 }}>
                     <div>
                         <p>{notif.title}</p>
-                        <p style={{ fontSize: "12px", color: "#8c8c8c" }}>{notif.time}</p>
+                        <span style={{ fontSize: "12px", color: "#8c8c8c" }}>{notif.time}</span>
                     </div>
                 </div>
-            }>
-            </SubMenu>
+            </Menu.Item>
+
         )
     })
 
     const menu = (
-        <Menu mode="vertical" trigger={['click']} >
+        <Menu multiple mode="inline" trigger={['click']} selectable={false} selectable={false}  style={{
+            width: "100%",
+            maxHeight: "calc(100vh - 200px)",
+            overflowY: "auto",
+            overflowX: "hidden",
+            backgroundColor: "#fff",
+            borderRadius: "5px",
+            boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.1)",
+            border: "1px solid #e8e8e8",
+            }}>
             {mapnotifs}
-        </Menu>
+      </Menu>
     );
 
     return (
-        <Dropdown overlay={menu} trigger={['click']} >
-            <div className="ant-dropdown-link" style={{padding: '8px 0px', cursor: 'pointer'}} >
-                <Badge count={notifs.length} showZero className="site-badge-count-109" style={{backgroundColor: "#6FCF97", color: '#fff', margin: 0, padding: 0}} >
-                    <BellOutlined style={{fontSize: 25, margin: 0, padding: 0}} />
+        <div style={{
+            position: "relative",
+            display: "flex",
+            alignItems: "center",
+            marginTop: "5px",
+        }}>
+            <Popover
+                content={menu}
+                trigger="click"
+                placement="bottomRight"
+                overlayClassName="notif-popover"
+                autoAdjustOverflow={false}
+                // getPopupContainer={trigger => trigger.parentNode}
+                // arrowContent={null}
+                title={
+                    <p style={{ fontSize: "16px", color: "#8c8c8c" }}>Notifications</p>
+                }
+            >
+                <Badge count={lenght} showZero className="site-badge-count-109" style={{cursor: "pointer", border: "1px solid #d9d9d9",backgroundColor: "#6FCF97", color: '#fff', margin: 0, padding: 0}} >
+                    <BellOutlined style={{fontSize: 25, margin: 0, padding: 0, cursor: 'pointer'}} />
                 </Badge>
-            </div>
-        </Dropdown>
+            </Popover>
+        </div>
+        
+
+
+
     );
 }
 
